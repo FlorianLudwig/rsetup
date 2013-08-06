7bcb0b7eccd1327496713685ded7b54e936c9582
Florian Ludwig <f.ludwig@greyrook.com>
2013-08-06 16:37:03 +0200

    upate

--- rsetup/cli.py
+++ rsetup/cli.py
@@ -2,9 +2,12 @@
 import os
 import logging
 import argparse
+import glob
+import shutil
 
+import setuptools
 import subprocess
-from rsetup.common import exe
+from rsetup import proc
 
 
 ARG_PARSER = argparse.ArgumentParser(description=__doc__,
@@ -26,18 +29,72 @@ TEST_PKGS = ['GitPython==0.3.2.RC1',
 ]
 
 
+def get_setup_data(path):
+    data = {}
+
+    old_setup = setuptools.setup
+
+    def s(**kwargs):
+        data.update(kwargs)
+
+    setuptools.setup = s
+    exec compile(open(path).read(), path, 'exec') in {'__file__': path}, {}
+    setuptools.setup = old_setup
+    return data
+
+
+def get_python_interpreter(args):
+    if 'PYTHON_EXE' in os.environ:
+        return os.environ['PYTHON_EXE']
+    return 'python'
+
+
+
+@command
+def sdist(args):
+    python = get_python_interpreter(args)
+    if os.path.exists('dist'):
+        shutil.rmtree('dist')
+    proc.exe([python, 'setup.py', 'sdist', '--dev'])
+    if args.ci:
+        proc.exe(['pip', 'install'] + glob.glob('dist/*.tar.gz'))
+
+sdist.parser.add_argument('--ci', action='store_true',
+                          help='running in CI context')
+
+
+@command
+def test(args):
+    setup_data = get_setup_data('setup.py')
+    pkgs = setup_data['packages']
+    pkgs = set(pkg.split('.')[0] for pkg in pkgs)
+    pkgs = list(pkgs)
+
+    py_test = ['py.test', '--cov' '.']
+    if args.ci:
+        py_test += ['--cov-report', 'xml', '--junitxml=junit.xml']
+    proc.exe(py_test)
+
+    pylint = ['pylint', '-f', 'parseable'] + pkgs
+    pylint_out = proc.read(pylint)
+    open('pylint_out', 'w').write(pylint_out)
+
+    proc.exe(['coverage', 'html'])
+test.parser.add_argument('--ci', action='store_true',
+                         help='running in CI context')
+
+
 @command
 def setup(args):
-    proc = subprocess.Popen(['pip', 'install'] + TEST_PKGS)
-    proc.wait()
+    p = subprocess.Popen(['pip', 'install'] + TEST_PKGS)
+    p.wait()
 
-    pkgs = exe(['pip', 'freeze'])
+    pkgs = proc.read(['pip', 'freeze'])
     before = open('pip_freeze_before_install.txt', 'w')
     for line in pkgs.split('\n'):
         if not line.startswith('rsetup') or line.startswith('configobj'):
             before.write(line + '\n')
     before.close()
-
 setup.parser.add_argument('--ci', action='store_true',
                           help='running in CI context')
 
--- rsetup/common.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-
-def exe(cmd):
-    stdout, _ = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
-    return stdout
